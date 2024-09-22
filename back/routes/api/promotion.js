1f13f0c536886babb4050592496da9a5ae66c6c4
koiescafe2024 <nicheelee24@gmail.com>
2024-09-22 16:05:12 +0530

    Update promotion.js

--- back/routes/api/promotion.js
+++ back/routes/api/promotion.js
@@ -11,12 +11,9 @@ const Promotion = require("../../models/Promotion");
 const User = require("../../models/User");
 
 
-router.get("/getpromotions", async (req, res) => {
+router.get("/getpromotions", auth,async (req, res) => {
     try {
-        let promotionsList = await Promotion.find({
-            agentname: "luckyama",
-           
-        }).limit(50);
+        let promotionsList = await Promotion.limit(50);
         console.log("data....."+promotionsList)
         res.json({ status: "0000", promotionsList });
     } catch (err) {
