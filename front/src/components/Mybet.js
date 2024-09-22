7c83de738a71110b040cf441d47326bf366662df
koiescafe2024 <nicheelee24@gmail.com>
2024-09-22 16:58:54 +0530

    Update Mybet.js

--- front/src/components/Mybet.js
+++ front/src/components/Mybet.js
@@ -75,7 +75,7 @@ export const MyBet = () => {
         <div className="max-w-[1200px] m-auto w-full mt-[30px] px-4 md:px-0">
             <div className="flex justify-between items-center mb-4">
                 <h1 className="text-lg font-semibold text-[var(--secondaryColor)]">
-                    My Bet
+                    My Betsss
                 </h1>
                 <div className="flex items-center">
                     <select
